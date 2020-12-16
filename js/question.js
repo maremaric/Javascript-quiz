2112966e2c29f2a5350f1de8c889d3ec7827eaf8
maremaric <markoreus888@gmail.com>
2020-12-16 17:55:38 +0100

    vezba

--- js/question.js
+++ js/question.js
@@ -2,29 +2,85 @@
 // Array of Objects
 const quiz = [
     {
-        q: 'Which month comes right before june ?',
-        options: ['may','september','july','august'],
-        answer: 0
-    },
-    {
-        q: 'What color is a banana',
-        options: ['red','yellow','white','blue'],
+        q: 'Anatema je:',
+        options: ['Blagoslov', 'Prokletstvo', 'Prevara', 'Presuda'],
         answer: 1
     },
     {
-        q: '3 + 4 = 7',
-        options: ['true','false'],
-        answer: 0
-    },
-    {
-        q: 'What time of day do we have breakfast ?',
-        options: ['In the afternoon','In the evening','In the morning'],
+        q: 'Koji broj je sledeci u nizu brojeva: 1, 4, 9, 16, 25?',
+        options: ['33','35','36','39'],
         answer: 2
     },
     {
-        q: 'What is 22 + 6',
-        options: ['99','56','16','28'],
+        q: 'Mizantrop:',
+        options: ['Voli ljude','Nema osecanja','Potpuno je smusen','Mrzi ljude'],
+        answer: 3
+    },
+    {
+        q: 'Kako se zvao prvi Zemljin satelit koji je bio lansiran iz SSSR-a 1957. godine?',
+        options: ['Sputnjik I','Sputnjik II','Apollo 11','Vostok'],
+        answer: 0
+    },
+    {
+        q: 'Pored toga sto je osnovao fond iz kojeg ce svake godine dodeljivati nagrade u vise kategorija(Nodelova nagrada), Alfred Nobel je poznat i kao covek koji je izumeo i patentirao:',
+        options: ['Aspirin','Plastiku','Dinamit','Hemijsku olovku'],
+        answer: 2
+    },
+    {
+        q: 'U kojoj drzavi se nalazi najvise vulkana?',
+        options: ['Filipini', 'Indonezija', 'Japan', 'Nikaragva'],
+        answer: 1
+    },
+    {
+        q: 'Koji je film iz 1991. godine osvojio Oskara za najbolji adaptirani scenarij,najbolji film,najboljeg glumca,glumicu i reditelja?',
+        options: ['Kad jaganjci utihnu','Robin Hood: Princ lopova','Porodica Adams','Terminator 2: Sudnji dan'],
+        answer: 0
+    },
+    {
+        q: 'Afrika nije prirodno staniste ove zivotinje:',
+        options: ['Tigar','Gnu','Leopard','Kapski pavijan'],
+        answer: 0
+    },
+    {
+        q: 'Koji fudbaler je dobio nagradu za najboljeg strelca svetskog prvenstva u Brazilu 2014?',
+        options: ['Cristiano Ronaldo','Thomas Muller','James Rodriguez','Lionel Messi'],
+        answer: 2
+    },
+    {
+        q: 'Drugi svetski rat je zavrsen:',
+        options: ['25. maja 1945','5. novembra 1945','2. septembra 1945','7. oktobra 1945'],
+        answer: 2
+    },
+    {
+        q: 'Koji srpski pisac je napisao cuvenu trilogiju Perunov Hronicar',
+        options: ['Vladimir Petkovic', 'Milos Petkovic', 'Marko Maric', 'Mladen Djordjevic'],
+        answer: 1
+    },
+    {
+        q: 'Akrofobicni ljudi se boje:',
+        options: ['Visine','Pauka','Uskog prostora','Nista od navedenog'],
+        answer: 0
+    },
+    {
+        q: 'Koji od navedenih fudbalera ima najvise osvojenih trofeja u karijeri?',
+        options: ['Zinedine Zidane','Lionel Messi','Dani Alves','Maradona'],
+        answer: 2
+    },
+    {
+        q: 'Koji poznati fudbaler ima nadimak El Matador?',
+        options: ['Edinson Cavani','Arturo Vidal','Angel Di Maria','Thiago Silva'],
+        answer: 0
+    },
+    {
+        q: 'Koji klub je najtrofejniji u Engleskoj',
+        options: ['Manchester United','Arsenal','Chelsea','Liverpool'],
         answer: 3
     }
 ];
 
+
+// {
+//     q: '3 + 4 = 7',
+//     options: ['true','false'],
+//     answer: 0
+// }
